98be4682c35552b4f7a34086509a45820f1861ea
PlayitLOUD73 <github@lukewelborn.com>
2022-02-08 18:18:23 -0500

    Added asset loading into exectuable

--- raylib.go
+++ raylib.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"embed"
+	_ "embed"
+	"io/fs"
 	"os"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
@@ -10,13 +13,31 @@ var background rl.Texture2D
 var title rl.Texture2D
 var button1 rl.Texture2D
 
+//go:embed "assets/*.png"
+var files embed.FS
+
 // Init initializes a raylib window and sets the appropiate FPS target.
 func Init() {
 	rl.InitWindow(800, 600, "Hackman")
 	rl.SetTargetFPS(60)
-	background = rl.LoadTexture("assets/background.png")
-	title = rl.LoadTexture("assets/title.png")
-	button1 = rl.LoadTexture("assets/button1.png")
+
+	//var Background []byte = assets[0].Name()
+
+	assets, _ := fs.ReadDir(files, "assets")
+	//var Background []byte
+	//var name string = assets[0].Name()
+	backgroundRaw, _ := fs.ReadFile(files, "assets/"+assets[0].Name())
+	var backgroundImg *rl.Image = rl.LoadImageFromMemory(".png", backgroundRaw, int32(len(backgroundRaw)))
+	background = rl.LoadTextureFromImage(backgroundImg)
+
+	button1Raw, _ := fs.ReadFile(files, "assets/"+assets[1].Name())
+	var button1Img *rl.Image = rl.LoadImageFromMemory(".png", button1Raw, int32(len(button1Raw)))
+	button1 = rl.LoadTextureFromImage(button1Img)
+
+	titleRaw, _ := fs.ReadFile(files, "assets/"+assets[2].Name())
+	var titleImg *rl.Image = rl.LoadImageFromMemory(".png", titleRaw, int32(len(titleRaw)))
+	title = rl.LoadTextureFromImage(titleImg)
+
 }
 
 // drawLetter draws an individual letter at the given position.
